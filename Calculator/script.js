deb4dceaca219da511c9c290772eae3f5eea3d4d
HayiderHasan18 <hayiderhasan26@gmail.com>
2025-02-20 21:41:46 +0300

    some

--- Calculator/script.js
+++ Calculator/script.js
@@ -124,8 +124,8 @@ date.addEventListener("click", () => {
   result.className = "result";
 });
 time.addEventListener("click", () => {
-  let time1 = new time();
+  let date1 = new date();
   let result = document.getElementById("result");
-  result.innerText = `the time now is \n \n ${time1.toLocaleTimeString()}`;
+  result.innerText = `the time now is \n \n ${date1.toLocaleTimeString()}`;
   result.className = "result";
-});12
+});
