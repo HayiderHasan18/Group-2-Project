559fc3469c77e19ff6dccea50f2d788008029266
kedish2man <kedirzemecha24@gmail.com>
2025-03-10 23:51:57 +0300

    display in the browser

--- PHP Project/index.php
+++ PHP Project/index.php
@@ -31,6 +31,12 @@ if (isset($_POST['login'])) {
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['email'] = $email;
+            echo "Login successful!";
 
     <!DOCTYPE html>
 <html lang="en">
