8ecaf8b5266054d659dea68aa2ace464a6d72dc4
kedish2man <kedirzemecha24@gmail.com>
2025-03-10 23:47:28 +0300

    validization part

--- PHP Project/index.php
+++ PHP Project/index.php
@@ -23,6 +23,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (isset($_POST['login'])) {
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
+    if (empty($email) || empty($password)) {
+        echo "Both fields are required!";
+        exit;
+    }
 
     <!DOCTYPE html>
 <html lang="en">
