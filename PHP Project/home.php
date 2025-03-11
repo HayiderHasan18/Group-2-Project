dce575ffcc4dff82c4af54eac553294e9cff236f
HayiderHasan18 <hayiderhasan26@gmail.com>
2025-03-11 13:46:24 +0300

    linked to css

--- PHP Project/home.php
+++ PHP Project/home.php
@@ -85,7 +85,7 @@ $tasks = mysqli_query($connection, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="./home.css">
+    <link rel="stylesheet" href="./Css/home.css">
 </head>
 <body>
 
